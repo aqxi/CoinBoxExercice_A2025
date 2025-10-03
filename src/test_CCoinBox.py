7baff7264e9b7df020395c5421a0df412c09d4cf
SamaiAzimi <samaiazimi17@gmail.com>
2025-10-03 14:00:14 -0400

    Created test mutant

--- src/test_CCoinBox.py
+++ src/test_CCoinBox.py
@@ -27,3 +27,27 @@ class Test_CCoinBox(unittest.TestCase):
         coinBox.ajouter_25c()
         coinBox.vente()
         self.assertEqual(coinBox.get_vente_permise(), True)
+
+    def test_vente_deduit_correctement_monnaie(self):
+        """Test pour s'assurer qu'une vente SOUSTRAIT la monnaie et ne l'ADDITIONNE pas"""
+        coinBox = CCoinBox()
+        # Ajouter 4 pièces pour avoir assez pour une vente
+        coinBox.ajouter_25c()  # 1 pièce
+        coinBox.ajouter_25c()  # 2 pièces
+        coinBox.ajouter_25c()  # 3 pièces
+        coinBox.ajouter_25c()  # 4 pièces
+        
+        # Vérifier l'état avant vente
+        monnaie_avant = coinBox.get_monnaie_courante()
+        self.assertEqual(monnaie_avant, 4)
+        
+        # Effectuer une vente
+        coinBox.vente()
+        
+        # Vérifier que la monnaie a DIMINUÉ (pas augmenté)
+        monnaie_apres = coinBox.get_monnaie_courante()
+        self.assertEqual(monnaie_apres, 2)  # 4 - 2 = 2
+        
+        # S'assurer que la monnaie après vente est inférieure à avant
+        self.assertLess(monnaie_apres, monnaie_avant, 
+                       "La monnaie courante doit diminuer après une vente, pas augmenter!")
